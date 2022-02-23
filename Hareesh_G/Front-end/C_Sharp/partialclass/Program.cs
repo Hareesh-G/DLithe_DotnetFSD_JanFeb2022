89ac463cc7e721efbfab3723cf783179f87b7158
G Hareesh <86514434+Hareesh-G@users.noreply.github.com>
2022-02-23 20:23:31 +0530

    Add files via upload

---
+++ Hareesh_G/Front-end/C_Sharp/partialclass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HeightWeightInfo
+{
+    class Company
+    {
+    }
+    public partial class Employee
+    {
+        private int salary;
+        private int age;
+        public Employee(int salary, int age)
+        {
+            this.salary = salary;
+            this.age = age;
+        }
+    }
+    public partial class Student
+    {
+        public partial void InitializeStudent();
+        public void Height() { }
+        public void Weight() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee(300000, 25);
+            myEmployee.PrintEmployee();
+            Student c = new Student();
+            c.InitializeStudent();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hareesh_G/Front-end/C_Sharp/partialclass/partialclass1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightWeightInfo
+{
+    internal class partialclass1
+    {
+    }
+    public partial class Student
+    {
+        public void Bu() { }
+        public partial void InitializeStudent()
+        {
+            string str = "Student";
+            Console.WriteLine(str);
+        }
+    }
+    public partial class Employee
+    {
+        public void PrintEmployee()
+        {
+            Console.WriteLine("salary:" + salary);
+            Console.WriteLine("age:" + age);
+        }
+    }
+}
+
